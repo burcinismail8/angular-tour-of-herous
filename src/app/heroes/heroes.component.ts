91b2d38bbb967726a001d1dcef6558106a5494a5
Burchin Ismail <burcinismail8@gmail.com>
2022-06-14 01:13:12 +0300

    Added services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.addMessage(
+      `HeroesComponent: Selected hero id=${hero.id}`
+    );
   }
-  constructor() {}
 
-  ngOnInit(): void {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes: Hero[]) => {
+      this.heroes = heroes;
+    });
+  }
 }
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+})
+export class MessagesComponent implements OnInit {
+  constructor(public messageService: MessageService) {}
+  ngOnInit(): void {}
+}
